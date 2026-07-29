d44bc12a40d7adc403120603e458606e110fba7b
rhectoroc <rhectoroc@gmail.com>
2026-07-29 14:20:45 -0400

    feat: add VehicleForm component with Zod validation and update dependencies

--- src/components/VehicleForm.jsx
+++ src/components/VehicleForm.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { useForm, useWatch } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
